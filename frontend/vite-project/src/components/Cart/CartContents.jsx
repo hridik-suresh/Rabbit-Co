666ea83703b90d8907f0275a50fe2d2b7db00e03
hridik-suresh <iamhridiksuresh@gmail.com>
2025-11-08 18:54:19 +0530

    created the demo for the cart items and aligned it properly

--- frontend/vite-project/src/components/Cart/CartContents.jsx
+++ frontend/vite-project/src/components/Cart/CartContents.jsx
@@ -1,9 +1,75 @@
-
+import { RiDeleteBin3Line } from "react-icons/ri";
 
 function CartContents() {
-    return (
-        <div>Cart items will go here</div>
-    )
+  const cartProducts = [
+    {
+      productId: 1,
+      name: "T-shirt",
+      size: "M",
+      color: "Red",
+      quantity: 1,
+      price: 15,
+      image: "https://picsum.photos/200?random=1",
+    },
+    {
+      productId: 2,
+      name: "T-shirt",
+      size: "M",
+      color: "Red",
+      quantity: 1,
+      price: 15,
+      image: "https://picsum.photos/200?random=2",
+    },
+    {
+      productId: 3,
+      name: "T-shirt",
+      size: "M",
+      color: "Red",
+      quantity: 1,
+      price: 15,
+      image: "https://picsum.photos/200?random=3",
+    },
+  ];
+  return (
+    <div>
+      {cartProducts.map((product, index) => {
+        return (
+          <div key={index} className="flex items-start justify-between py-4 border-b">
+            <div className="flex">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-20 h-24 object-cover mr-4 rounded"
+              />
+              <div>
+                <h3>{product.name}</h3>
+                <p className="text-sm text-gray-500">
+                  size: {product.size} | color: {product.color}
+                </p>
+                <div className="flex  items-center mt-2">
+                  <button className="border flex items-center justify-center rounded px-2 py-s text-sm font-medium">
+                    +
+                  </button>
+                  <span className="mx-4 border rounded px-2 flex items-center justify-center">
+                    {product.quantity}
+                  </span>
+                  <button className="border flex items-center justify-center rounded px-2 py-s text-sm font-medium">
+                    -
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <p className="font-medium">$ {product.price.toLocaleString()}</p>
+              <button>
+                <RiDeleteBin3Line className="size-6 mt-2 text-red-600" />
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CartContents;
--- frontend/vite-project/src/components/Layout/CartDrawer.jsx
+++ frontend/vite-project/src/components/Layout/CartDrawer.jsx
@@ -4,7 +4,7 @@ import CartContents from "../Cart/CartContents";
 function CartDrawer({ isDrawerOpen, toggleCartDrawer }) {
   return (
     <div
-      className={`fixed top-0 right-0 h-full w-3/4 sm:w-1/2 md:w-1/4 bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
+      className={`fixed top-0 right-0 h-full w-3/4 sm:w-1/2 md:w-[30rem] bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
         isDrawerOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
@@ -13,7 +13,7 @@ function CartDrawer({ isDrawerOpen, toggleCartDrawer }) {
           <IoMdClose className="size-6 text-gray-600" />
         </button>
       </div>
-      <div className="flex-grow p-4 overflow-y-auto">
+      <div className="grow p-4 overflow-y-auto">
         <h2 className="text-xl font-semibold mb-4">Shopping Cart</h2>
         {/* Cart items will go here */}
         <CartContents />
