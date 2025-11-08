0f125872a3dfc32e630653f83a4094b925987f33
hridik-suresh <iamhridiksuresh@gmail.com>
2025-11-08 21:52:03 +0530

    Footer is completed.

--- frontend/vite-project/src/components/Layout/UserLayout.jsx
+++ frontend/vite-project/src/components/Layout/UserLayout.jsx
@@ -1,12 +1,15 @@
+import Footer from "../Common/Footer";
 import Header from "../Common/Header";
 
   
 
 function UserLayout() {
   return (
-    <div>
-      <Header/>
-    </div>
+    <>
+      <Header />
+      
+      <Footer/>
+    </>
   );
 }
 
