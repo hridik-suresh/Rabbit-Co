2c8f490a519c1d6f9d6207848d4695f3cf4a30e8
hridik-suresh <iamhridiksuresh@gmail.com>
2025-11-09 17:24:19 +0530

    Created the demo data for new arrivals.

---
+++ frontend/vite-project/src/components/Products/NewArrivals.jsx
@@ -0,0 +1,98 @@
+
+
+function NewArrivals() {
+    const newArrivals = [
+        {
+            _id: 1,
+            name: "Product 1",
+            image: [
+                {
+                    url: "https://picsum.photos/500/500?random=1",
+                    alt: "Product 1 Image",
+                },
+            ],
+            price: "$49.99",
+        },
+        {
+            _id: 2,
+            name: "Product 2",
+            image: [
+                {
+                    url: "https://picsum.photos/500/500?random=2",
+                    alt: "Product 2 Image",
+                },
+            ],
+            price: "$29.99",
+        },
+        {
+            _id: 3,
+            name: "Product 3",
+            image: [
+                {
+                    url: "https://picsum.photos/500/500?random=3",
+                    alt: "Product 3 Image",
+                },
+            ],
+            price: "$39.99",
+        },
+        {
+            _id: 4,
+            name: "Product 4",
+            image: [
+                {
+                    url: "https://picsum.photos/500/500?random=4",
+                    alt: "Product 4 Image",
+                },
+            ],
+            price: "$59.99",
+        },
+        {
+            _id: 5,
+            name: "Product 5",
+            image: [    
+                {
+                    url: "https://picsum.photos/500/500?random=5",
+                    alt: "Product 5 Image",
+                },
+            ],
+            price: "$69.99",
+        },
+        {
+            _id: 6,
+            name: "Product 6",
+            image: [
+                {
+                    url: "https://picsum.photos/500/500?random=6",
+                    alt: "Product 6 Image",
+                },
+            ],
+            price: "$79.99",
+        },
+        {
+            _id: 7,
+            name: "Product 7",
+            image: [
+                {
+                    url: "https://picsum.photos/500/500?random=7",
+                    alt: "Product 7 Image",
+                },
+            ],
+            price: "$89.99",
+        },
+        {
+            _id: 8,
+            name: "Product 8",
+            image: [
+                {
+                    url: "https://picsum.photos/500/500?random=8",
+                    alt: "Product 8 Image",
+                },
+            ],
+            price: "$99.99",
+        },
+    ];
+
+    return <div>New Arrivals Component</div>;
+}
+
+export default NewArrivals;
--- frontend/vite-project/src/pages/Home.jsx
+++ frontend/vite-project/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
+import NewArrivals from "../components/Products/NewArrivals";
 
 function Home() {
   return (
-        <div>
-          <Hero />
-          <GenderCollectionSection />
-        </div>
+    <div>
+      <Hero />
+      <GenderCollectionSection />
+      <NewArrivals />
+    </div>
   );
-}   
+}
 
 export default Home;
