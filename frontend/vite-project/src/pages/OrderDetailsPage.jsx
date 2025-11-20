d40ec1f740e2632444661916bd3a7a530a5809d6
hridik-suresh <iamhridiksuresh@gmail.com>
2025-11-20 22:21:12 +0530

    Order details page is completed.

--- frontend/vite-project/src/pages/OrderDetailsPage.jsx
+++ frontend/vite-project/src/pages/OrderDetailsPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function OderDetailsPage() {
   const { id } = useParams();
@@ -10,7 +10,7 @@ function OderDetailsPage() {
       _id: id,
       createdAt: new Date(),
       isPaid: true,
-      isDelivered: false,
+      isDelivered: true,
       paymentMethod: "PayPal",
       shippingMethod: "Standard",
       shippingAddress: { city: "New York", country: "USA" },
@@ -53,7 +53,7 @@ function OderDetailsPage() {
             </div>
             <div className="flex flex-col items-start sm:items-end mt-4 sm:mt-0">
               <span
-                className={` ${
+                className={`${
                   orderDetails.isPaid
                     ? "bg-green-100 text-green-700"
                     : "bg-red-100 text-red-700"
@@ -61,8 +61,75 @@ function OderDetailsPage() {
               >
                 {orderDetails.isPaid ? "Approved" : "Pending"}
               </span>
+              <span
+                className={`${
+                  orderDetails.isDelivered
+                    ? "bg-yellow-100 text-yellow-700"
+                    : "bg-red-100 text-red-700"
+                } px-3 py-1 rounded-full text-sm font-medium mb-2 `}
+              >
+                {orderDetails.isDelivered ? "Delivered" : "Pending"}
+              </span>
             </div>
           </div>
+          {/* Customer, Payment, Shipping Info */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-8">
+            <div>
+              <h4 className="text-lg font-semibold mb-2 ">Payment Info</h4>
+              <p>Payment Method: {orderDetails.paymentMethod}</p>
+              <p>Status: {orderDetails.isPaid ? "Paid" : "Unpaid"}</p>
+            </div>
+            <div>
+              <h4 className="text-lg font-semibold mb-2 ">Shipping Info</h4>
+              <p>Shipping Method: {orderDetails.shippingMethod}</p>
+              <p>
+                Address:{" "}
+                {`${orderDetails.shippingAddress.city},  ${orderDetails.shippingAddress.country}`}
+              </p>
+            </div>
+          </div>
+          {/* Product List */}
+          <div className="overflow-x-auto">
+            <h4 className="text-lg font-semibold mb-4">Products</h4>
+            <table className="min-w-full text-gray-600 mb-4">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="py-2 px-4">Name</th>
+                  <th className="py-2 px-4">Unit Price</th>
+                  <th className="py-2 px-4">Quantity</th>
+                  <th className="py-2 px-4">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orderDetails.orderItems.map((item) => (
+                  <tr key={item.productId} className="border-b">
+                    <td className="py-2 px-4 flex items-center justify-center">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="size-12 object-cover rounded-lg mr-4"
+                      />
+                      <Link
+                        to={`/products/${item.productId}`}
+                        className="text-blue-500 hover:underline"
+                      >
+                        {item.name}
+                      </Link>
+                    </td>
+                    <td className="py-2 text-center px-4">{item.price}</td>
+                    <td className="py-2 text-center px-4">{item.quantity}</td>
+                    <td className="py-2 text-center px-4">
+                      {item.price * item.quantity}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          {/* Back to Order link */}
+          <Link to="my-orders" className="text-blue-500 hover:underline">
+            Back to My Orders
+          </Link>
         </div>
       )}
     </div>
