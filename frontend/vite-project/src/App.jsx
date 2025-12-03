f3b8cb8dd036df8ada1f0cc9b366f5e9b1b584e0
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-03 13:52:06 +0530

    Route for the page to edit the product for the admin pannel is created.

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -15,6 +15,7 @@ import AdminLayout from "./components/Admin/AdminLayout.jsx";
 import AdminHomePage from "./pages/AdminHomePage.jsx";
 import UserManagement from "./components/Admin/UserManagement.jsx";
 import ProductManangement from "./components/Admin/ProductManangement.jsx";
+import EditProductPage from "./components/Admin/EditProductPage.jsx";
 
 function App() {
   return (
@@ -42,6 +43,7 @@ function App() {
           <Route index element={<AdminHomePage />} />
           <Route path="users" element={<UserManagement />} />
           <Route path="products" element={<ProductManangement />} />
+          <Route path="products/:id/edit" element={<EditProductPage />} />
         </Route>
       </Routes>
     </div>
---
+++ frontend/vite-project/src/components/Admin/EditProductPage.jsx
@@ -0,0 +1,5 @@
+const EditProductPage = () => {
+  return <div className="max-w-3xl mx-auto p-6"> dshjfgas</div>;
+};
+
+export default EditProductPage;
