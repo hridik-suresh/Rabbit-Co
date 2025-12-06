b4d3290a2e74afc934c5f4e364d36174b9c4bcba
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-06 16:06:09 +0530

    The logic for creating the JWT token for the users while registering is written successfully.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,35 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const  jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 //@route POST /api/usera/register
 //@desc Register a new user
 //@access Public
-router.post('/register', async (req, res)=>{
-    const {name, email, password} = req.body;
-    try{
-       //Register user
-       let user = await User.findOne({email});
-       if(user) return res.status(400).json({message: "User already exist"});
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    //Register user
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exist" });
 
-       user = new User({name, email, password});
-       await user.save();
+    user = new User({ name, email, password });
+    await user.save();
 
-       res.status(201).json({
-        user: {
+    //Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    //Sign and Return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "10h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //Send the User and Token in response
+        res.status(201).json({
+          user: {
             _id: user._id,
             name: user.name,
             email: user.email,
             role: user.role,
-        }
-       })
-
-    }catch(error){
-       console.log(error);
-         res.status(500).send('Server Error'); 
-    }
-})
-
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
