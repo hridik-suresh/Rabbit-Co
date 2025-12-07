a435f953a3bd51597a932953cbd7e762785a3a83
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-07 17:59:57 +0530

    PUT /api/products/:id route  to update the existing product is created

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -61,4 +61,68 @@ router.post("/", protect, async (req, res) => {
   }
 });
 
+//@route PUT /api/products/:id
+//@desc Update a product
+//@access Private (Admin only)
+router.put("/:id", protect, async (req, res) => {
+  if (req.user && req.user.role === "admin") {
+    try {
+      const product = await Product.findById(req.params.id);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      // Update product fields
+      const {
+        name,
+        description,
+        price,
+        discountPrice,
+        countInStock,
+        category,
+        brand,
+        sizes,
+        colors,
+        collections,
+        material,
+        gender,
+        images,
+        isFeatured,
+        isPublished,
+        dimensions,
+        weight,
+        sku,
+      } = req.body;
+
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isPublished : product.isPublished;
+      product.dimensions = dimensions || product.dimensions;
+      product.weight = weight || product.weight;
+      product.sku = sku || product.sku;
+
+      await product.save();
+      res.json(product);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server Error");
+    }
+  } else {
+    res.status(403).json({ message: "Access denied" });
+  }
+});
+
 module.exports = router;
