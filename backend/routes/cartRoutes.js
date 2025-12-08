32c3a0973303acf9b30ad14919dd44371a297f4c
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-08 21:32:30 +0530

    POST /api/cart/merge route is created to aloow users to add products to the cart as a guest and during the checkout, allow the user to login and checkout the products.

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -192,4 +192,64 @@ router.get("/", async (req, res) => {
     }
 });
 
+
+//@route POST /api/cart/merge
+//@desc Merge guest cart into logged-in user's cart upon login
+//@access Public
+router.post("/merge", async (req, res) => {
+  const { guestId } = req.body;
+
+    try {
+        const guestCart = await Cart.findOne({guestId});
+        const userCart = await Cart.findOne({user: req.user._id});
+
+        if (guestCart) {
+            if(guestCart.products.length === 0){
+                return res.status(400).json({message: "Guest cart is empty"});
+            }
+
+            if(userCart){
+                //Merge guest cart into user cart
+                guestCart.products.forEach(guestItem => {
+                    const userItemIndex = userCart.products.findIndex(
+                        item => 
+                            item.productId.toString() === guestItem.productId.toString() &&
+                            item.size === guestItem.size &&
+                            item.color === guestItem.color
+                    );
+
+                    if(userItemIndex > -1){
+                        //If product exists in user cart, update quantity
+                        userCart.products[userItemIndex].quantity += guestItem.quantity;
+                    } else {
+                        //If product does not exist, add to user cart
+                        userCart.products.push(guestItem);
+                    }
+                });
+
+                //Recalculate total price
+                userCart.totalPrice = userCart.products.reduce(
+                    (total, item) => total + item.price * item.quantity,
+                    0
+                );
+
+                await userCart.save();
+                await guestCart.remove();
+                res.status(200).json(userCart);
+            } else {
+                //If user cart does not exist, assign guest cart to user
+                guestCart.user = req.user._id;
+                guestCart.guestId = undefined;
+                await guestCart.save();
+                res.status(200).json(guestCart);
+            }
+        } else {
+            res.status(404).json({message: "Guest cart not found"});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Server error"});
+    }
+});
+
 module.exports = router;
