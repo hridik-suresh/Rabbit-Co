937f45f225656abee28808aa218e7cc247401641
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-08 12:50:56 +0530

    Model for the cart is created.

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,41 +1,42 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
-const User = require('./models/User');
-const Product = require('./models/Product');
-const products = require('./data/products');
-
+const User = require("./models/User");
+const Product = require("./models/Product");
+const Cart = require("./models.Cart");
+const products = require("./data/products");
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL);
 
 //Seed data function
 const seedData = async () => {
-    try {
-        //Clear existing data
-        await Product.deleteMany();
-        await User.deleteMany();
+  try {
+    //Clear existing data
+    await Product.deleteMany();
+    await User.deleteMany();
+    await Cart.deleteMany();
 
-        //Create an admin user
-        const adminUser = new User({
-            name: 'Admin User',
-            email: 'admin@example.com',
-            password: 'adminpassword',
-            role: 'admin',
-        });
-        await adminUser.save();
+    //Create an admin user
+    const adminUser = new User({
+      name: "Admin User",
+      email: "admin@example.com",
+      password: "adminpassword",
+      role: "admin",
+    });
+    await adminUser.save();
 
-        //Assign the default userID to each product
-        const sampleProducts = products.map(product => {
-            return { ...product, user: adminUser._id };
-        });
-        await Product.insertMany(sampleProducts);
-        console.log('Data seeded successfully');
-        process.exit();
-    } catch (error) {
-        console.error('Error seeding data:', error);
-        process.exit(1);
-    }
+    //Assign the default userID to each product
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser._id };
+    });
+    await Product.insertMany(sampleProducts);
+    console.log("Data seeded successfully");
+    process.exit();
+  } catch (error) {
+    console.error("Error seeding data:", error);
+    process.exit(1);
+  }
 };
 
 seedData();
