8b272fb891e6a1413422329e4eb6caa2cb22929d
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-18 11:13:00 +0530

    PUT /api/admin/orders/:id route is created that let the admin to update the status of order.

--- backend/routes/adminOrderRoutes.js
+++ backend/routes/adminOrderRoutes.js
@@ -20,4 +20,40 @@ router.get("/", protect, async (req, res) => {
   }
 });
 
+//@route PUT /api/admin/orders/:id
+//@desc Update order status (Admin only)
+//@access Private
+router.put("/:id", protect, async (req, res) => {
+  if (req.user && req.user.role === "admin") {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    try {
+      const order = await Order.findById(id);
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+
+      if (status) {
+        order.status = status || order.status;
+        order.isDelivered = status === "Delivered" ? true : order.isDelivered;
+        order.deliveredAt =
+          status === "Delivered" ? Date.now() : order.deliveredAt;
+      }
+
+      await order.save();
+
+      res.json({
+        message: "Order status updated successfully",
+        order,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server Error");
+    }
+  } else {
+    res.status(403).json({ message: "Access denied" });
+  }
+});
+
 module.exports = router;
