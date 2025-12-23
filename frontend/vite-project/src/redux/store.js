d75d7d7e0bcf3c1d08ee16db7b47d42ef175b2a4
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-24 00:31:34 +0530

    Add products reducer to Redux store configuration

--- frontend/vite-project/src/redux/store.js
+++ frontend/vite-project/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"; // Make sure the path is correct
+import productReducer from "./slices/productsSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer, // This name "auth" must match what you use in useSelector
+    products: productReducer,
   },
 });
 
