a8e77ae5ab8874d8991eab31e64c91184174c7e3
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-25 01:49:15 +0530

    Add checkout reducer to Redux store configuration

--- frontend/vite-project/src/redux/store.js
+++ frontend/vite-project/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"; // Make sure the path is correct
 import productReducer from "./slices/productsSlice";
 import cartReducer from "./slices/cartSlice";
+import checkoutReducer from "./slices/checkoutSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer, // This name "auth" must match what you use in useSelector
     products: productReducer,
     cart: cartReducer,
+    checkout: checkoutReducer,
   },
 });
 
