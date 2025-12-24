a4ff453a1a4d3cc41d7eed37ef208fc644cad3ca
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-25 01:28:27 +0530

    Add cart reducer to Redux store configuration

--- frontend/vite-project/src/redux/store.js
+++ frontend/vite-project/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"; // Make sure the path is correct
 import productReducer from "./slices/productsSlice";
+import cartReducer from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer, // This name "auth" must match what you use in useSelector
     products: productReducer,
+    cart: cartReducer,
   },
 });
 
